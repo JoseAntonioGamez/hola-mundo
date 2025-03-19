031afbf704d9725020f41ddf6efd12a533c8a4bd
José Antonio Gámez <jose.gamez.granado.al@iespoligonosur.org>
2025-03-19 09:21:53 +0100

    Add files via upload

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,11 @@
+/**
+* Muestra por pantalla el mensaje "Hola Mundo"
+*
+* @author JoseAntonioGamezGranado
+*/
+
+
+
 public class HolaMundo {
 	public static void main(String[] args) {
 		system.out.println("¡Hola Mundo!");	
