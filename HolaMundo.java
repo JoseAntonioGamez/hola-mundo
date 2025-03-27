5689c600ea616833787af45cebfc7e6959544d84
Alumnado Mañana <jose.gamez.granado.al@iespoligonosur.org>
2025-03-27 13:08:20 +0100

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -8,6 +8,9 @@
 
 public class HolaMundo {
 	public static void main(String[] args) {
+		String naranja = "\033[33m";
+		String azul = "\033[34m";
+		
 		system.out.println("¡Hola Mundo!");
 		system.out.println("Aqui estamos haciendo pruebas con Git.");	
 	}
